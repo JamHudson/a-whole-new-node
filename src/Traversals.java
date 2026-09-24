a736c52bbb587a0143de875f71662bdf6f9a0ce9
JHud <219741840+JamHudson@users.noreply.github.com>
2026-09-24 11:42:37 -0700

    inclass

--- src/Traversals.java
+++ src/Traversals.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Traversals {
   public static void main(String[] args) {
     Node<Integer> root = new Node<>(88);
-
 
     Node<Integer> child1 = new Node<>(17);
     Node<Integer> child2 = new Node<>(58);
@@ -20,7 +24,6 @@ public class Traversals {
     child1.children.add(child1_1);
     child1.children.add(child1_2);
 
-
     Node<Integer> child2_1 = new Node<>(73);
     child2.children = new ArrayList<>();
     child2.children.add(child2_1);
@@ -33,11 +36,67 @@ public class Traversals {
     child3.children.add(child3_2);
     child3.children.add(child3_3);
 
-
     Node<Integer> child3_1_1 = new Node<>(83);
     Node<Integer> child3_1_2 = new Node<>(6);
     child3_1.children = new ArrayList<>();
     child3_1.children.add(child3_1_1);
     child3_1.children.add(child3_1_2);
+    //
+
+    Map<Integer,List<Integer>> tree = new HashMap<>();
+    tree.put(88, List.of(17,58,33));
+    tree.put(17, List.of(5,99));
+    tree.put(58, List.of(73));
+    tree.put(33, List.of(24,61,12));
+    tree.put(24, List.of(83,6));
+
+    // preorder(root);
+    // preorderMap(tree, 88);
+    System.out.println(byLevel(root));
+    System.out.println(sum(root));
+  }
+
+  public static <T> void preorderMap(Map<T,List<T>> tree, T node) {
+    if (tree == null || node == null) return;
+    System.out.println(node);
+
+    if (tree.containsKey(node));
+    for (T child : tree.get(node)) preorderMap(tree, child);
+  }
+
+  public static void preorder(Node<?> node) {
+    if (node == null) return;
+    System.out.println(node.value);
+
+    for (Node<?> child : node.children) preorder(child);
+  }
+
+  public static int sum(Node<Integer> node) {
+    if (node == null) return 0;
+    int sum = node.value;
+    
+    for (Node<Integer> child : node.children) sum += sum(child);
+    return sum;
+  }
+
+  public static <T> List<List<T>> byLevel(Node<T> root) {
+    List<List<T>> levelOrder = new ArrayList<>();
+    if (root == null) return levelOrder;
+    Queue<Node<T>> queue = new LinkedList<>();
+
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      List<T> level = new ArrayList<T>();
+      for (int i=0;i<size;i++) {
+        Node<T> node = queue.poll();
+        level.add(node.value);
+        for (Node<T> child : node.children) queue.offer(child);
+      }
+      levelOrder.add(level);
+    }
+
+    return levelOrder;
   }
 }
